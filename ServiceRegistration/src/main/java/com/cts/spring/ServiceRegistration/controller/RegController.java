e6c80fe957b1cf4c8b5a26333b3ebaad34c06469
ashok <ashokpasupu@gmail.com>
2019-09-21 08:26:54 +0530

    Added Controller

---
+++ ServiceRegistration/src/main/java/com/cts/spring/ServiceRegistration/controller/RegController.java
@@ -0,0 +1,17 @@
+package com.cts.spring.ServiceRegistration.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Reg/v1/api")
+public class RegController {
+
+	@GetMapping("/getdata")
+
+	public String GetData() {
+		return "welCome to Spring ";
+	}
+
+}
