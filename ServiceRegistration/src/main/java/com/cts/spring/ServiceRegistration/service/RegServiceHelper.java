8ca8112cf72106750b443190c5115c90169d6529
ashok <ashokpasupu@gmail.com>
2019-09-22 15:18:32 +0530

    Added Helper Class

---
+++ ServiceRegistration/src/main/java/com/cts/spring/ServiceRegistration/service/RegServiceHelper.java
@@ -0,0 +1,13 @@
+package com.cts.spring.ServiceRegistration.service;
+
+import org.springframework.stereotype.Service;
+
+@Service("RegServiceHelper")
+public class RegServiceHelper {
+
+	public String getData() {
+		// TODO Auto-generated method stub
+		return "welcome to Spring ";
+	}
+
+}
